ae53be7dfbd6a6a83a24d7ea7b8b58d0177e3579
Alejandro Builes <alebg2510@hotmail.com>
2023-06-13 11:55:10 -0500

    new changes, script prints by chapter and yml file not done.

--- generate_learning_outcomes.py
+++ generate_learning_outcomes.py
@@ -4,15 +4,26 @@ import re
 # GitHub repository URL
 repo_url = "https://github.com/OpenIntroStat/openintro-statistics-learn-obj"
 
-# Directory numbers from 00 to 09
-directories = [str(x).zfill(2) for x in range(10)]
+# Directory numbers from 00 to 09 along with their corresponding chapter names
+directories = {
+    "00": "OpenIntro: Statistics",
+    "01": "Chapter 1: Introduction to Data",
+    "02": "Chapter 2: Summarizing Data",
+    "03": "Chapter 3: Probability",
+    "04": "Chapter 4: Distributions of random variables",
+    "05": "Chapter 5: Foundations for inference",
+    "06": "Chapter 6: Inference for categorical data",
+    "07": "Chapter 7: Inference for numerical data",
+    "08": "Chapter 8: Introduction to linear regression",
+    "09": "Chapter 9: Multiple and logistic regression"
+}
 
 # Regular expression pattern to extract learning outcomes
 pattern = r"\\item(.*)"
 
 # Iterate over directories and retrieve learning outcomes
-learning_outcomes = []
-for directory in directories:
+chapter_learning_outcomes = {}
+for directory, chapter_name in directories.items():
     # Construct the URL for the .tex file
     file_url = f"{repo_url}/blob/master/{directory}/{directory}learn_obj.tex"
     raw_file_url = file_url.replace('/blob/', '/raw/')
@@ -25,14 +36,18 @@ for directory in directories:
         # Extract learning outcomes using regular expression
         matches = re.findall(pattern, content)
         
-        # Append learning outcomes to the list
-        learning_outcomes.extend(matches)
+        # Store chapter learning outcomes
+        chapter_learning_outcomes[chapter_name] = matches
     except urllib.error.HTTPError as e:
         print(f"Failed to retrieve file from URL: {file_url}")
         print(f"HTTP Error {e.code}: {e.reason}")
         print()
         continue
 
-# Print the extracted learning outcomes
-for index, outcome in enumerate(learning_outcomes, start=1):
-    print(f"Learning Outcome {index}: {outcome.strip()}")
+# Print the extracted learning outcomes by chapter
+for chapter_name, outcomes in chapter_learning_outcomes.items():
+    print(chapter_name)
+    print("-" * len(chapter_name))
+    for index, outcome in enumerate(outcomes, start=1):
+        print(f"Learning Outcome {index}: {outcome.strip()}")
+    print()
